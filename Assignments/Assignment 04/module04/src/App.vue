b58cf607662f20651ca1b614fc3d3f491126ce5f
Mobarak Ali <mobarak.ali@gmail.com>
2023-07-22 16:15:59 +0600

    test 01

--- Assignments/Assignment 04/module04/src/App.vue
+++ Assignments/Assignment 04/module04/src/App.vue
@@ -1,47 +1,47 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+const carouselImages = [
+  { src: 'https://picsum.photos/id/236/1200/500', alt: 'First slide' },
+  { src: 'https://picsum.photos/id/237/1200/500', alt: 'Second slide' },
+  { src: 'https://picsum.photos/id/238/1200/500', alt: 'Third slide' },
+];
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div>
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li v-for="(image, index) in carouselImages" :key="index" :data-target="'#carouselExampleIndicators'" :data-slide-to="index" :class="{ 'active': index === 0 }"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div v-for="(image, index) in carouselImages" :key="index" :class="{ 'carousel-item': true, 'active': index === 0 }">
+          <img class="d-block w-100" :src="image.src" :alt="image.alt">
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+
+
+
+<style>
+/* Optional custom CSS styles for the carousel */
+.carousel {
+  margin: 0 auto; 
+  max-width: 1200px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.carousel img {
+  max-width: 100%;
 }
 </style>
+
+<!-- Add Bootstrap CSS link to the index.html file -->
