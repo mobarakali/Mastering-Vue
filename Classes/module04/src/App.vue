2fb905d58f56fa07de1a3775cf2d984f5fae7f9b
Mobarak Ali <mobarak.ali@gmail.com>
2023-07-23 23:21:13 +0600

    Live test

--- Classes/module04/src/App.vue
+++ Classes/module04/src/App.vue
@@ -1,54 +1,21 @@
-<script setup>
-// const items = [1,2,3,4,5];
-const tasks =[
-  {id:1, title:'Wakeup Early @ 4AM', status:"In Progress"},
-  {id:2, title:'Perform Fajr Salat', status:"In Progress"},
-  {id:3, title:'Morning Walk', status:"In Progress"},
-  {id:4, title:'Start Office', status:"Completed"},
-  {id:5, title:'Break Fast', status:"In Progress"}
-]
-
-function getCompletedTasks (){
-  // return tasks.filter(task=>"Completed" ==task.status);
-  return tasks.filter(task=>"Completed" == task.status)
-} 
-function getTasksInProgress (){
-  return tasks.filter(task=>"In Progress" ==task.status);
-} 
-
-</script>
-
 <template>
   <div>
-    <h2>Task Completed</h2>
-    <ul>
-      <!-- <li v-for="item in items">{{ item }} </li> -->
-      <!-- <li v-for="task in tasks" :key="task.id">{{ task.title }} </li> -->
-      <li v-for="task in getCompletedTasks()" :key="task.id">{{ task.title }} </li>
-    </ul>
-    <h2>Task In Pregress</h2>
-    <ul>
-      <li v-for="task in getTasksInProgress()" :key="task.id">{{ task.title }} </li>
-    </ul>
-
-    <div style="margin: 10px auto ;" v-for="(task, index) in tasks" :key="index">
-      {{ task.id }}. {{task.title }}
-    </div>
+    <VForArrayComponent />
   </div>
-  
 </template>
 
+<script>
+// import VForArrayComponent from './path/to/VForArrayComponent.vue';
+import VForArrayComponent from './components/VForArrayComponent.vue';
+
+export default {
+  components: {
+    VForArrayComponent
+  }
+};
+</script>
+
+
 <style scoped>
-ul{
-  list-style-type: none;
-  min-width: 300px;
-}
-li{
-  background-color: #ffffff;
-  margin: 2px auto;
-  color: #000000;
-  padding:15px;
-  font-size: 1.125rem;
-}
 
 </style>
---
+++ Classes/module04/src/components/VForArrayComponent.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+      <ul>
+        <li v-for="item in items" :key="item.id">
+          <span class="bold">{{ item.title }}</span> - {{ item.description }}
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  import { reactive } from 'vue';
+  
+  export default {
+    setup() {
+      const items = reactive([
+        {
+          id: 1,
+          title: "Item 1",
+          description: "Description for Item 1"
+        },
+        {
+          id: 2,
+          title: "Item 2",
+          description: "Description for Item 2"
+        },
+        {
+          id: 3,
+          title: "Item 3",
+          description: "Description for Item 3"
+        },
+        {
+          id: 4,
+          title: "Item 4",
+          description: "Description for Item 4"
+        },
+        {
+          id: 5,
+          title: "Item 5",
+          description: "Description for Item 5"
+        },
+        {
+          id: 6,
+          title: "Item 6",
+          description: "Description for Item 6"
+        }
+      ]);
+  
+      return {
+        items
+      };
+    }
+  };
+  </script>
+  
+<style scoped>
+.bold{
+    font-weight: bold;
+}
+</style>
