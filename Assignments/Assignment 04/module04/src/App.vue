dc28bbc421e4fafa41c0970b95948f17b35a6e30
Mobarak Ali <mobarak.ali@gmail.com>
2023-07-23 10:34:48 +0600

    Module 04 in the Classes

--- Assignments/Assignment 04/module04/src/App.vue
+++ Assignments/Assignment 04/module04/src/App.vue
@@ -1,47 +1,73 @@
-<script setup>
-const carouselImages = [
-  { src: 'https://picsum.photos/id/236/1200/500', alt: 'First slide' },
-  { src: 'https://picsum.photos/id/237/1200/500', alt: 'Second slide' },
-  { src: 'https://picsum.photos/id/238/1200/500', alt: 'Third slide' },
-];
-</script>
-
 <template>
-  <div>
-    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-      <ol class="carousel-indicators">
-        <li v-for="(image, index) in carouselImages" :key="index" :data-target="'#carouselExampleIndicators'" :data-slide-to="index" :class="{ 'active': index === 0 }"></li>
-      </ol>
-      <div class="carousel-inner">
-        <div v-for="(image, index) in carouselImages" :key="index" :class="{ 'carousel-item': true, 'active': index === 0 }">
-          <img class="d-block w-100" :src="image.src" :alt="image.alt">
+  <div class="container">
+    <div class="row">
+      <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"></li>
+          <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"></li>
+          <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div
+            class="carousel-item"
+            v-for="(image, index) in images"
+            :key="index"
+            :class="{ active: index === 0 }"
+          >
+            <img class="d-block w-100" :src="image.src" :alt="image.alt">
+          </div>
         </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </a>
       </div>
-      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
     </div>
   </div>
 </template>
 
+<script setup>
+const jsonData = {
+  "images": [
+    {
+      "src": "https://picsum.photos/id/247/1920/650",
+      "alt": "First slide"
+    },
+    {
+      "src": "https://picsum.photos/id/257/1920/650",
+      "alt": "Second slide"
+    },
+    {
+      "src": "https://picsum.photos/id/237/1920/650",
+      "alt": "Third slide"
+    }
+  ]
+};
 
+const { images } = jsonData;
+</script>
 
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  setup() {
+    // No need for explicit setup function here, as it's included in the <script setup> block above.
+  }
+});
+</script>
 
 <style>
-/* Optional custom CSS styles for the carousel */
-.carousel {
-  margin: 0 auto; 
-  max-width: 1200px;
+#app{
+  display: initial;
+  max-width: initial;
 }
-
-.carousel img {
-  max-width: 100%;
+.carousel-indicators{
+  list-style-type: none;
 }
+/* Add any custom styles here if needed. */
 </style>
-
-<!-- Add Bootstrap CSS link to the index.html file -->
