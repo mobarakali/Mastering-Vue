ceb318b56218ba1bf69a9b5ba85a3da49083d0a9
Mobarak Ali <mobarak.ali@gmail.com>
2023-07-23 23:22:36 +0600

    Updated Assignment data

--- Assignments/Assignment 04/module04/src/App.vue
+++ Assignments/Assignment 04/module04/src/App.vue
@@ -1,23 +1,33 @@
 
+
 <script setup>
-const carouselData = {
-  "images": [
-    {
-      "src": "https://picsum.photos/id/247/1920/650",
-      "alt": "First slide"
-    },
-    {
-      "src": "https://picsum.photos/id/257/1920/650",
-      "alt": "Second slide"
-    },
-    {
-      "src": "https://picsum.photos/id/237/1920/650",
-      "alt": "Third slide"
-    }
-  ]
+import { ref } from 'vue';
+
+const carouselData = [
+  {
+    "src": "https://picsum.photos/id/200/1920/650",
+    "alt": "First slide"
+  },
+  {
+    "src": "https://picsum.photos/id/1000/1920/650",
+    "alt": "Second slide"
+  },
+  {
+    "src": "https://picsum.photos/id/1040/1920/650",
+    "alt": "Third slide"
+  }
+];
+
+const images = carouselData;
+const currentSlideIndex = ref(0);
+
+const nextSlide = () => {
+  currentSlideIndex.value = (currentSlideIndex.value + 1) % images.length;
 };
 
-const { images } = carouselData;
+const prevSlide = () => {
+  currentSlideIndex.value = (currentSlideIndex.value - 1 + images.length) % images.length;
+};
 </script>
 
 <template>
@@ -25,25 +35,18 @@ const { images } = carouselData;
     <div class="row">
       <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
         <ol class="carousel-indicators">
-          <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"></li>
-          <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"></li>
-          <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"></li>
+          <li v-for="(image, index) in images" :key="index" data-bs-target="#carouselExampleIndicators" :data-bs-slide-to="index" :class="{ active: index === currentSlideIndex }" ></li>
         </ol>
         <div class="carousel-inner">
-          <div
-            class="carousel-item"
-            v-for="(image, index) in images"
-            :key="index"
-            :class="{ active: index === 0 }"
-          >
+          <div class="carousel-item" v-for="(image, index) in images" :key="index" :class="{ active: index === currentSlideIndex }" >
             <img class="d-block w-100" :src="image.src" :alt="image.alt">
           </div>
         </div>
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-bs-slide="prev">
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-bs-slide="prev" @click="prevSlide">
           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
           <span class="visually-hidden">Previous</span>
         </a>
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-bs-slide="next">
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-bs-slide="next" @click="nextSlide">
           <span class="carousel-control-next-icon" aria-hidden="true"></span>
           <span class="visually-hidden">Next</span>
         </a>
@@ -54,11 +57,11 @@ const { images } = carouselData;
 
 
 <style>
-#app{
+#app {
   display: initial;
   max-width: initial;
 }
-.carousel-indicators{
+.carousel-indicators {
   list-style-type: none;
 }
 /* Add any custom styles here if needed. */
